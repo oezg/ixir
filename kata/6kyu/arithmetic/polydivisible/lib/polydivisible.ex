2697237b33caab3494cb8ab4d409e95c09a54ad6
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-05-20 00:39:09 +0200

    poly

--- kata/6kyu/arithmetic/polydivisible/lib/polydivisible.ex
+++ kata/6kyu/arithmetic/polydivisible/lib/polydivisible.ex
@@ -1,44 +1,28 @@
 defmodule Polydivisible do
   @moduledoc false
-  @max_polydivisible 3_608_528_850_368_400_786_036_725
 
   def next(n) do
+    polydivisibles()
+    |> Enum.find(&(&1 > n))
   end
 
-  def next_polydivisible(n) do
-    if n < 10 do
-      n + 1
-    else
-    end
-
-    len = count_digits(n)
-
-    if count_digits(next_divisible_by_length(n)) > len do
-    end
+  def polydivisibles do
+    Stream.unfold(
+      {1, 1..9 |> Enum.to_list()},
+      fn
+        {_, []} -> nil
+        {digit, polys} -> {polys, {digit + 1, next(digit, polys)}}
+      end
+    )
+    |> Enum.to_list()
+    |> List.flatten()
   end
 
-  def find_polydivisible(n) do
-    Stream.iterate(1, &next_divisible_by_length/1)
-    |> Stream.filter(&polydivisible?/1)
-    |> Stream.drop_while(&(&1 <= n))
-    |> Enum.take(1)
-    |> List.first()
-  end
-
-  def polydivisible?(n) when is_integer(n) do
-    cond do
-      n < 10 -> true
-      rem(n, count_digits(n)) > 0 -> false
-      true -> polydivisible?(div(n, 10))
-    end
-  end
-
-  def next_divisible_by_length(n) do
-    len = count_digits(n)
-    n + len - rem(n, len)
-  end
-
-  def count_digits(n) do
-    ceil(:math.log(n) / :math.log(10))
+  def next(digit, polys) do
+    for polydivisible <- polys,
+        increment <- 0..9,
+        candidate = polydivisible * 10 + increment,
+        rem(candidate, digit + 1) == 0,
+        do: candidate
   end
 end
