d82d843b01577f34a1418ee7c0898bc9711567b1
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-05-19 13:46:28 +0200

    poly

--- kata/6kyu/arithmetic/polydivisible/lib/polydivisible.ex
+++ kata/6kyu/arithmetic/polydivisible/lib/polydivisible.ex
@@ -2,25 +2,46 @@ defmodule Polydivisible do
   @moduledoc false
 
   def next(n) do
-    if n < 1 do
-      1
+    if polydivisible?(n) do
+      next(n + 1)
     else
-      if 3_608_528_850_368_400_786_036_725 <= n do
-        nil
-      else
-        Stream.iterate(n + 1, &next_divisible_by_length/1)
-        |> Stream.drop_while(&is_not_polydivisible/1)
-        |> Enum.take(1)
-        |> List.first()
+      m = div(n, 10)
+
+      if polydivisible?(m) do
       end
+    end
+
+    # if n < 1 do
+    #   1
+    # else
+    #   if 3_608_528_850_368_400_786_036_725 <= n do
+    #     nil
+    #   else
+    #     find_polydivisible(n)
+    #   end
+    # end
+  end
+
+  def next_polydivisible(n) do
+    len = count_digits(n)
+
+    if count_digits(next_divisible_by_length(n)) > len do
     end
   end
 
-  def is_not_polydivisible(n) when is_integer(n) do
+  def find_polydivisible(n) do
+    Stream.iterate(1, &next_divisible_by_length/1)
+    |> Stream.filter(&polydivisible?/1)
+    |> Stream.drop_while(&(&1 <= n))
+    |> Enum.take(1)
+    |> List.first()
+  end
+
+  def polydivisible?(n) when is_integer(n) do
     cond do
-      n < 10 -> false
-      rem(n, count_digits(n)) > 0 -> true
-      true -> is_not_polydivisible(div(n, 10))
+      n < 10 -> true
+      rem(n, count_digits(n)) > 0 -> false
+      true -> polydivisible?(div(n, 10))
     end
   end
 
