cf49dac234a0da25d7fb99c6a261726787725908
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-05-26 12:06:56 +0200

    alpha substring

--- kata/6kyu/strings/longest_alphabetical_substring/lib/longest_alphabetical_substring.ex
+++ kata/6kyu/strings/longest_alphabetical_substring/lib/longest_alphabetical_substring.ex
@@ -1,19 +1,39 @@
 defmodule LongestAlphabeticalSubstring do
-  @moduledoc false
+  @moduledoc """
+  Find the longest substring in alphabetical order.
+  """
 
+  @spec longest(binary()) :: binary()
   def longest(string) do
-    String.codepoints(string)
+    {i, n} = search(string, 0, 0, 0, 0, "")
+    String.slice(string, i, n)
   end
 
-  def longest_alphabetical_prefix(codepoints, len) do
-    if len == length(codepoints) do
-      codepoints
+  defp search("", cur_idx, cur_len, max_idx, max_len, _old_chr) do
+    if cur_len > max_len do
+      {cur_idx, cur_len}
     else
-      if Enum.at(codepoints, len - 1) < Enum.at(codepoints, len) do
-        longest_alphabetical_prefix(codepoints, len + 1)
-      else
-        Enum.slice(codepoints, len)
-      end
+      {max_idx, max_len}
+    end
+  end
+
+  defp search(
+         <<cur_chr::binary-size(1), rest::binary>>,
+         cur_idx,
+         cur_len,
+         max_idx,
+         max_len,
+         old_chr
+       ) do
+    cond do
+      cur_chr >= old_chr ->
+        search(rest, cur_idx, cur_len + 1, max_idx, max_len, cur_chr)
+
+      cur_len > max_len ->
+        search(rest, cur_idx + cur_len, 1, cur_idx, cur_len, cur_chr)
+
+      true ->
+        search(rest, cur_idx + cur_len, 1, max_idx, max_len, cur_chr)
     end
   end
 end
