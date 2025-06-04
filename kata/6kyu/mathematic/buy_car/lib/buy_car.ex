dc4bed682d66ecefa3bf53b5ab7b8a320af48549
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-06-04 11:45:07 +0200

    buy car

--- kata/6kyu/mathematic/buy_car/lib/buy_car.ex
+++ kata/6kyu/mathematic/buy_car/lib/buy_car.ex
@@ -5,18 +5,19 @@ defmodule BuyCar do
   """
 
   def nb_months(startPriceOld, startPriceNew, savingPerMonth, percentLossByMonth) do
-    Stream.unfold({0, startPriceOld - startPriceNew, startPriceOld - startPriceNew}, fn
-      {_month, _diff, available} when available >= 0 ->
-        nil
+    while(startPriceOld - startPriceNew, savingPerMonth, percentLossByMonth, 0)
+  end
 
-      {month, diff, available} ->
-        {
-          {month + 1, round(available)},
-          {month + 1, diff * (1 - (percentLossByMonth + div(month, 2) * 0.5) / 100),
-           diff + savingPerMonth * month + savingPerMonth}
-        }
-    end)
-    |> Enum.to_list()
-    |> List.last()
+  def loss(percentLossByMonth, monthCount), do: percentLossByMonth + div(monthCount + 1, 2) * 0.5
+  def loose(difference, loss_percent), do: difference * (1 - loss_percent / 100)
+
+  def while(difference, spm, plbm, nbm) do
+    available = difference + spm * nbm
+
+    if available >= 0 do
+      {nbm, round(available)}
+    else
+      while(loose(difference, loss(plbm, nbm)), spm, plbm, nbm + 1)
+    end
   end
 end
