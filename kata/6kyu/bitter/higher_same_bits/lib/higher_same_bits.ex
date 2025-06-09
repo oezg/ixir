5a9e13cfba6f3681c49dfbcbde9c9d27109671ac
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-06-09 23:42:09 +0200

    larger number with same set bit count

--- kata/6kyu/bitter/higher_same_bits/lib/higher_same_bits.ex
+++ kata/6kyu/bitter/higher_same_bits/lib/higher_same_bits.ex
@@ -4,13 +4,20 @@ defmodule HigherSameBits do
   I.e. as many 1 bits as before. Input is always an int between 1 and 1<<30
   """
 
-  def next_higher(n) when is_integer(n) and n > 0 do
-    bit_count = fn x -> for(<<bit::1 <- :binary.encode_unsigned(x)>>, do: bit) |> Enum.sum() end
-    target = bit_count.(n)
+  @spec next_higher(pos_integer()) :: pos_integer()
+  def next_higher(n),
+    do: Integer.digits(n, 2) |> Enum.reverse() |> next_higher({0, 0}) |> Integer.undigits(2)
 
-    Stream.iterate(n + 1, fn x -> x + 1 end)
-    |> Stream.filter(fn x -> target == bit_count.(x) end)
-    |> Stream.take(1)
-    |> Enum.at(0)
-  end
+  defp next_higher([], {zeros, ones}),
+    do: [1 | List.duplicate(0, zeros + 1)] ++ List.duplicate(1, ones - 1)
+
+  defp next_higher([], acc), do: acc
+  defp next_higher([1 | tail], {zeros, ones}), do: next_higher(tail, {zeros, ones + 1})
+
+  defp next_higher([0 | tail], {zeros, 0}), do: next_higher(tail, {zeros + 1, 0})
+
+  defp next_higher([0 | tail], {zeros, ones}),
+    do: next_higher(tail, next_higher([], {zeros, ones}))
+
+  defp next_higher([head | tail], acc), do: next_higher(tail, [head | acc])
 end
