2bebd335eb200a9932f5925076f67febdcef469b
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-07-05 00:37:47 +0200

    thue morse

--- kata/6kyu/bitter/thue_morse_sequence/lib/thue_morse_sequence.ex
+++ kata/6kyu/bitter/thue_morse_sequence/lib/thue_morse_sequence.ex
@@ -1,6 +1,17 @@
 defmodule ThueMorseSequence do
   @moduledoc false
 
+  def thue(1), do: "0"
+
   def thue(n) do
+    {a, b} = n |> :math.log2() |> ceil |> morse
+    String.slice(a <> b, 0, n)
+  end
+
+  def morse(1), do: {"0", "1"}
+
+  def morse(n) do
+    {a, b} = morse(n - 1)
+    {a <> b, b <> a}
   end
 end
