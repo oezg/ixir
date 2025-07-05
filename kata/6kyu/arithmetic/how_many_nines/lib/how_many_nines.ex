391f2e76289089397cbce6201b96d514b165f742
Özgür Yıldırım <ozguryenihayat@gmail.com>
2025-07-06 00:43:15 +0200

    nines

--- kata/6kyu/arithmetic/how_many_nines/lib/how_many_nines.ex
+++ kata/6kyu/arithmetic/how_many_nines/lib/how_many_nines.ex
@@ -1,17 +1,17 @@
 defmodule HowManyNines do
-  @moduledoc false
+  @moduledoc """
+  How many integers in the range [0..n] contain at least one 9 in their decimal representation?
+  """
 
-  def nines(n) do
-    t = :math.log10(n) |> floor() |> nine()
-    z = Integer.digits(n)
+  @spec nines(integer()) :: integer()
+  def nines(0), do: 0
+  def nines(n), do: nines(Integer.digits(n), floor(:math.log10(n)), 0)
+  defp nines([], _exp, acc), do: acc
+  defp nines([9 | tail], exp, acc), do: acc + 9 * nine(exp) + Integer.undigits(tail) + 1
+  defp nines([head | tail], exp, acc), do: nines(tail, exp - 1, acc + head * nine(exp))
 
-    # Enum.count(0..n, &Enum.member?(Integer.digits(&1), 9))
-  end
+  defp nine(0), do: 0
+  defp nine(1), do: 1
 
-  def nine(0), do: 0
-  def nine(1), do: 1
-
-  def nine(n) do
-    10 ** (n - 1) + 9 * nine(n - 1)
-  end
+  defp nine(n), do: 10 ** (n - 1) + 9 * nine(n - 1)
 end
